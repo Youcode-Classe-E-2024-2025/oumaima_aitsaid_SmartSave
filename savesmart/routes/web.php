7741cae86484eb6a0717201689664eb8bbaa826e
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-02-28 14:50:44 +0000

    ajouter middleware pour protéger la route /home

--- savesmart/routes/web.php
+++ savesmart/routes/web.php
@@ -13,4 +13,4 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/home', [AuthController::class, 'home'])->name('home'); 
+Route::get('/home', [AuthController::class, 'home'])->name('home')->middleware('auth'); 
